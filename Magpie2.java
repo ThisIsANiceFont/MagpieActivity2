223889d5153f415d184f52c7c7e561d4a9be65e8
ThisIsANiceFont <54092225+ThisIsANiceFont@users.noreply.github.com>
2019-10-23 10:33:04 -0700

    Add files via upload

--- Magpie2.java
+++ Magpie2.java
@@ -42,15 +42,18 @@ public class Magpie2
         {
             response = "Tell me more about your family.";
         }
-        else if (statement.indexOf("cat") >= 0
+        //code from part B for pets
+        /*else if (statement.indexOf("cat") >= 0
                 || statement.indexOf("dog") >= 0)
         {
             response = "Tell me more about your pets.";
-        }
+        }*/
+        //code from part B for teacher name
+        /*
         else if (statement.indexOf("Kaehms") >= 0)
         {
             response = "He's really living life to the fullest, huh!";
-        }
+        }*/
         else if (statement.indexOf("Hello") >= 0)
         {
             response = "Hi! Ask me a question!";
@@ -67,6 +70,17 @@ public class Magpie2
         {
             response = "Say something, please.";
         }
+        // PLTW 1.1.6 Part C start
+        else if (findKeyword(statement, "cat") >= 0
+                || findKeyword(statement, "dog") >= 0)
+        {
+            response = "Tell me more about your pets.";
+        }
+        else if (findKeyword(statement, "Kaehms") >= 0)
+        {
+            response = "He's really living life to the fullest, huh!";
+        }
+        // PLTW 1.1.6 Part C end
         else
         {
             response = getRandomResponse();
@@ -111,5 +125,90 @@ public class Magpie2
         }
 
         return response;
+    }
+    
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no").
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @param startPos
+     *            the character of the string to begin the
+     *            search at
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal,
+    int startPos)
+    {
+        String phrase = statement.trim();
+        // The only change to incorporate the startPos is in
+        // the line below
+        int psn = phrase.toLowerCase().indexOf(
+                goal.toLowerCase(), startPos);
+
+        // Refinement--make sure the goal isn't part of a
+        // word
+        while (psn >= 0)
+        {
+            // Find the string of length 1 before and after
+            // the word
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn)
+                .toLowerCase();
+            }
+            if (psn + goal.length() < phrase.length())
+            {
+                after = phrase.substring(
+                    psn + goal.length(),
+                    psn + goal.length() + 1)
+                .toLowerCase();
+            }
+            
+            // If before and after aren't letters, we've
+            // found the word
+            if (((before.compareTo("a") < 0) || (before
+                    .compareTo("z") > 0)) // before is not a
+                // letter
+            && ((after.compareTo("a") < 0) || (after
+                    .compareTo("z") > 0)))
+            {
+                return psn;
+            }
+
+            // The last position didn't work, so let's find
+            // the next, if there is one.
+            psn = phrase.indexOf(goal.toLowerCase(),
+                psn + 1);
+
+        }
+
+        return -1;
+    }
+    
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no"). The search
+     * begins at the beginning of the string.
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal)
+    {
+        return findKeyword(statement, goal, 0);
     }
 }
